9e4564b83985fbc771713ed225f6635588b02b61
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-05-05 16:50:00 +0100

    added react-icons library to project

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import './navbar.css';
+import React from "react";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <div className="container nav__container">
+        <ul className="nav__items">
+          <li>
+            <a href="index.html">Home</a>
+          </li>
+          <li>
+            <a href="index.html#about">About</a>
+          </li>
+          <li>
+            <a href="index.html#testimonials">Testimonials</a>
+          </li>
+          <li>
+            <a href="index.html#faqs">Faqs</a>
+          </li>
+          <li>
+            <a href="contact.html">Contact</a>
+          </li>
+        </ul>
+        <a href="index.html" className="nav__logo">
+          <h3>SmartHome</h3>
+        </a>
 
-export default Navbar
+        <div className="nav__signin-signup">
+          <a href="#">Login</a>
+          <a href="#">Sighup</a>
+        </div>
+        <button className="menu-btn"></button>
+        <button className="close-btn"></button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
