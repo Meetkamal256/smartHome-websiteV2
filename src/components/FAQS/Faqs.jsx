d460134c34b76855f264e5c974fa62f0aa6ee7cd
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-05-06 21:38:02 +0100

    FAQS component markup done

--- src/components/FAQS/Faqs.jsx
+++ src/components/FAQS/Faqs.jsx
@@ -1,10 +1,87 @@
-import React from 'react';
-import './faqs.css';
+import React from "react";
+import "./faqs.css";
+import { FaPlus } from "react-icons/fa";
 
 const Faqs = () => {
   return (
-    <div>Faqs</div>
-  )
-}
+    <section id="faqs">
+      <h1>FAQS</h1>
+      <div className="container faqs__container">
+        <article className="faq">
+          <span className="faq__icon">
+            <FaPlus />
+          </span>
+          <div className="faq__question-answer">
+            <h4 className="faq__question">How does this work?</h4>
+            <p className="faq__answer">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptatum, vel.
+            </p>
+          </div>
+        </article>
+        <article className="faq">
+          <span className="faq__icon">
+            <FaPlus />
+          </span>
+          <div className="faq__question-answer">
+            <h4 className="faq__question">How does this work?</h4>
+            <p className="faq__answer">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptatum, vel.
+            </p>
+          </div>
+        </article>
+        <article className="faq">
+          <span className="faq__icon">
+            <FaPlus />
+          </span>
+          <div className="faq__question-answer">
+            <h4 className="faq__question">How does this work?</h4>
+            <p className="faq__answer">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptatum, vel.
+            </p>
+          </div>
+        </article>
+        <article className="faq">
+          <span className="faq__icon">
+            <FaPlus />
+          </span>
+          <div className="faq__question-answer">
+            <h4 className="faq__question">How does this work?</h4>
+            <p className="faq__answer">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptatum, vel.
+            </p>
+          </div>
+        </article>
+        <article className="faq">
+          <span className="faq__icon">
+            <FaPlus />
+          </span>
+          <div className="faq__question-answer">
+            <h4 className="faq__question">How does this work?</h4>
+            <p className="faq__answer">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptatum, vel.
+            </p>
+          </div>
+        </article>
+        <article className="faq">
+          <span className="faq__icon">
+            <FaPlus />
+          </span>
+          <div className="faq__question-answer">
+            <h4 className="faq__question">How does this work?</h4>
+            <p className="faq__answer">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptatum, vel.
+            </p>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
 
-export default Faqs
+export default Faqs;
