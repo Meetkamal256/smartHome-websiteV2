f0d5d01c0d5df8eca81fd484d740790c1f2b99ce
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-05-06 14:54:54 +0100

    clients component jsx done

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
-import './clients.css';
+import React from "react";
+import "./clients.css";
+import client1 from "../../assets/client1.png";
+import client2 from "../../assets/client2.png";
+import client3 from "../../assets/client3.png";
+import client4 from "../../assets/client4.png";
+import client5 from "../../assets/client5.png";
+import client6 from "../../assets/client6.png";
+import client7 from "../../assets/client7.png";
+import client8 from "../../assets/client8.png";
 
 const Clients = () => {
   return (
-    <div>Clients</div>
-  )
-}
+    <section id="clients">
+      <h1>Our Clients</h1>
+      <p className="lead">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum veniam
+        eligendi cupiditate!
+      </p>
+      <div className="container clients__container">
+        <div>
+          <img src={client1} alt="" />
+        </div>
+        <div>
+          <img src={client2} alt="" />
+          <div>
+            <img src={client2} alt="" />
+          </div>
+          <div>
+            <img src={client3} alt="" />
+          </div>
+          <div>
+            <img src={client4} alt="" />
+          </div>
+          <div>
+            <img src={client5} alt="" />
+          </div>
+          <div>
+            <img src={client6} alt="" />
+          </div>
+          <div>
+            <img src={client7} alt="" />
+          </div>
+          <div>
+            <img src={client8} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Clients
+export default Clients;
