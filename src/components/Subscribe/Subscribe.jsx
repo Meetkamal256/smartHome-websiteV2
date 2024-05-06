7153ef757cbf34e8afd2a054b3cdcf75eb92ddde
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-05-07 00:22:13 +0100

    subscribe component markup and styling done

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import './subscribe.css';
+import React from "react";
+import "./subscribe.css";
 
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
-  )
-}
+    <section id="subscribe">
+      <div className="container subscribe__container">
+        <div className="subscribe__text">
+          <h2>Subscribe to Stay Updated</h2>
+          <p>
+            Don't miss out on the latest news and updates! Subscribe now to
+            receive notifications about our products and services.
+          </p>
+        </div>
+        <a href="#" className="btn subscribe__btn">
+          Get Updates
+        </a>
+      </div>
+    </section>
+  );
+};
 
-export default Subscribe
+export default Subscribe;
